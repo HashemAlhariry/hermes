65da1728ecafadfdc039e9f14252c143ecd832f8
Hashem Alhariry <hashemalhariry33@gmail.com>
2022-02-10 15:17:31 +0200

    small updates in chat area

--- hermes-client/src/main/java/gov/iti/jets/presentation/controllers/HomePageController.java
+++ hermes-client/src/main/java/gov/iti/jets/presentation/controllers/HomePageController.java
@@ -2,7 +2,6 @@ package gov.iti.jets.presentation.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -16,6 +15,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -34,38 +34,30 @@ public class HomePageController implements Initializable {
     private BorderPane mainBorderPane;
     @FXML
     private TextField tf_message;
-
     @FXML
     private Button button_send;
-
     @FXML
     private ImageView chattingScreen;
-
     @FXML
     private ImageView logout;
-
     @FXML
     private ImageView optionsOnChat;
-
     @FXML
     private ImageView profilePicture;
-
     @FXML
     private ImageView settings;
-
     @FXML
     private VBox vbox_messages;
-
     @FXML
     private ScrollPane sp_main;
-
     @FXML
     private Label labeltext;
     @FXML
     private Label labeltext2;
-
     @FXML
     private VBox mainVertical;
+    @FXML
+    private ImageView profileImage;
 
     @FXML
     void onSearchTextFieldClick(ActionEvent event) {
@@ -75,29 +67,31 @@ public class HomePageController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-       
+        // Sending message to vbox in chat box
         button_send.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 String messageToSend = tf_message.getText();
                 if (!messageToSend.isEmpty()) {
+                    ImageView imageView = new ImageView(profileImage.getImage());
+                    imageView.setFitWidth(18);
+                    imageView.setFitHeight(18);
 
                     HBox hBox = new HBox();
 
-                    System.out.print("ENTERED" + messageToSend);
                     hBox.setAlignment(Pos.CENTER_RIGHT);
                     hBox.setPadding(new Insets(5, 5, 5, 10));
 
                     Text text = new Text(messageToSend);
                     TextFlow textFlow = new TextFlow(text);
 
-                    textFlow.setStyle("-fx-color: rgb(239,242,255); " +
+                    textFlow.setStyle("-fx-color: rgb(255,255,255); " +
                             "-fx-background-color: rgb(15,125,242); " +
                             " -fx-background-radius: 20px; ");
                     textFlow.setPadding(new Insets(5, 10, 5, 10));
                     text.setFill(Color.color(0.934, 0.945, 0.996));
                     hBox.getChildren().add(textFlow);
-
+                    hBox.getChildren().add(imageView);
                     vbox_messages.getChildren().add(hBox);
 
                     // SEND MESSAGE TO SPECIFIC USER
