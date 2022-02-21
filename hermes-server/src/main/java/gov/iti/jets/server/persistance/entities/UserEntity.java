45fc594f1fa1ae44b041e3d6c6b095864c9a7df1
Hashem Alhariry <hashemalhariry33@gmail.com>
2022-02-21 22:33:11 +0200

    Update UserEntity.java

--- hermes-server/src/main/java/gov/iti/jets/server/persistance/entities/UserEntity.java
+++ hermes-server/src/main/java/gov/iti/jets/server/persistance/entities/UserEntity.java
@@ -1,16 +1,30 @@
 package gov.iti.jets.server.persistance.entities;
 
+import java.sql.Date;
+
 public class UserEntity {
+
 	public String phone;
 	public String name;
+	public String email;
+	public String password;
+	public String image;
+	public boolean gender;
+	public Date dob;
+	public String country;
+	public String bio;
 
-	public UserEntity() {
-
-	}
-
-	public UserEntity(String phone, String name) {
+	public UserEntity(String phone, String name, String email, String password, String image, boolean gender, Date dob,
+			String country, String bio) {
 		this.phone = phone;
 		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.image = image;
+		this.gender = gender;
+		this.dob = dob;
+		this.country = country;
+		this.bio = bio;
 	}
 
 	@Override
