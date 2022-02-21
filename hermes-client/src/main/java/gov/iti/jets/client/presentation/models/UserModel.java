3e5c7317fedf173f29ad82fb1115fb50d4d9c69b
Hashem Alhariry <hashemalhariry33@gmail.com>
2022-02-21 22:33:09 +0200

    Update UserModel.java

--- hermes-client/src/main/java/gov/iti/jets/client/presentation/models/UserModel.java
+++ hermes-client/src/main/java/gov/iti/jets/client/presentation/models/UserModel.java
@@ -1,14 +1,27 @@
 package gov.iti.jets.client.presentation.models;
 
+ 
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.scene.image.Image;
 
 public class UserModel {
+	// 	(Phone Number, Display Name, email, picture, password, confirmation password, 
+	// gender, country, date of birth, bio.)
 
 	private StringProperty userName = new SimpleStringProperty();
 	private StringProperty password = new SimpleStringProperty();
 	private StringProperty email = new SimpleStringProperty();
 	private StringProperty phoneNumber = new SimpleStringProperty();
+	private Property<Image> picture = new SimpleObjectProperty<>();
+	private StringProperty gender = new SimpleStringProperty();
+	private StringProperty country = new SimpleStringProperty();
+	private StringProperty dateOfBirth = new SimpleStringProperty();
+	private StringProperty bio = new SimpleStringProperty();
 
 	public String getUserName() {
 		return userName.get();
@@ -58,4 +71,84 @@ public class UserModel {
 		this.phoneNumber.set(phoneNumber);
 	}
 
+
+	public StringProperty bioProperty() {
+		return bio;
+	}
+
+	public String getBio() {
+		return bio.get();
+	}
+
+	public void setBio(StringProperty bio) {
+		this.bio = bio;
+	}
+
+	public void setBio(String bio) {
+		this.bio.set(bio);
+	}
+
+	public StringProperty countryProperty() {
+		return country;
+	}
+
+	public String getCountry() {
+		return country.get();
+	}
+
+	public void setCountry(StringProperty country) {
+		this.country = country;
+	}
+
+	public void setCountry(String country) {
+		this.country.set(country);
+	}
+
+	public StringProperty dateOfBirthProperty() {
+		return dateOfBirth;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth.get();
+	}
+
+	public void setDateOfBirth(StringProperty dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth.set(dateOfBirth);
+	}
+
+	public StringProperty genderpProperty() {
+		return gender;
+	}
+
+	public String getGender() {
+		return gender.get();
+	}
+
+	public void setGender(StringProperty gender) {
+		this.gender = gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender.set(gender);
+	}
+
+	public Property<Image> picturepProperty() {
+		return picture;
+	}
+
+	public Object getPicture() {
+		return picture.getBean();
+	}
+
+	public void setPicture(ObjectProperty picture) {
+		this.picture = picture;
+	}
+
+	public void setPicture(Image picture) {
+		this.picture.setValue(picture);
+	}
 }
