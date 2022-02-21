91da7111afbf4c02ab247fbc5446b98526d4bc8a
Hashem Alhariry <hashemalhariry33@gmail.com>
2022-02-21 22:33:15 +0200

    Update UserDto.java

--- hermes-common/src/main/java/common/business/dtos/UserDto.java
+++ hermes-common/src/main/java/common/business/dtos/UserDto.java
@@ -3,12 +3,30 @@ package common.business.dtos;
 import java.io.Serializable;
 
 public class UserDto implements Serializable {
-    
-    String phone;
-    String password;
-    public UserDto(String phone, String password) {
-        this.phone = phone;
+    // (Phone Number, Display Name, email, picture, password, confirmation password,
+    // gender, country, date of birth, bio.
+    public String phoneNumber;
+    public String name;
+    public String email;
+    public String password;
+    public String picture; 
+    public String gender;
+    public String dateOfBirth;
+    public String country;
+    public String bio;
+
+    public UserDto(String phoneNumber, String name, String email, String password, String picture, String gender,
+            String dateOfBirth, String country, String bio) {
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+        this.email = email;
         this.password = password;
+        this.picture = picture;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.country = country;
+        this.bio = bio;
     }
+ 
 
 }
