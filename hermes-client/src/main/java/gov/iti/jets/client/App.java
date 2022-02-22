6b5e293d06ed6359adb81aa2853e4605866f20b4
Sondos Salah <sondoss.salahh@gmail.com>
2022-02-22 18:48:26 +0200

    testing

--- hermes-client/src/main/java/gov/iti/jets/client/App.java
+++ hermes-client/src/main/java/gov/iti/jets/client/App.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 
 public class App extends Application {
 
-	private StageCoordinator stageCoordinator = StageCoordinator.INSTANCE;
+	private StageCoordinator stageCoordinator =    StageCoordinator.INSTANCE;
 
 	public static void main(String[] args) {
 
