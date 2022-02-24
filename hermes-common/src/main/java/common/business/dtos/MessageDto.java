bf8fcb930b8eb7874ec477eb3bef419a74ab48bb
amiraElmergawy <amiraelmergawy@gmail.com>
2022-02-24 18:57:30 +0200

    Update MessageDto.java

--- hermes-common/src/main/java/common/business/dtos/MessageDto.java
+++ hermes-common/src/main/java/common/business/dtos/MessageDto.java
@@ -2,14 +2,14 @@ package common.business.dtos;
 
 import java.io.Serializable;
 
-public class MessageDto implements Serializable{
-   public String senderPhone;
-   public String content;
-   public Long groupID;
-   
-public MessageDto(String senderPhone, String content, Long groupID) {
-    this.senderPhone = senderPhone;
-    this.content = content;
-    this.groupID = groupID;
-}
+public class MessageDto implements Serializable {
+    public String senderPhone;
+    public String content;
+    public Long groupID;
+
+    public MessageDto(String senderPhone, String content, Long groupID) {
+        this.senderPhone = senderPhone;
+        this.content = content;
+        this.groupID = groupID;
+    }
 }
