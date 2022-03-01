5cb21f1fc41fa2ad01cdc8949eba695c7adf8ff8
amiraElmergawy <amiraelmergawy@gmail.com>
2022-03-01 08:04:06 +0200

    Update MessageEntity.java

--- hermes-server/src/main/java/gov/iti/jets/server/persistance/entities/MessageEntity.java
+++ hermes-server/src/main/java/gov/iti/jets/server/persistance/entities/MessageEntity.java
@@ -7,13 +7,24 @@ public class MessageEntity {
 	public int id;
 	public String content;
 	public Date sendDate;
+	public String senderPhone;
+	public int receiverId;
 
 	public MessageEntity() {
 	}
 
-	public MessageEntity(int id, String content, Date sendDate) {
+	public MessageEntity(int id, String content, Date sendDate, String sender, int receiver) {
 		this.id = id;
 		this.content = content;
 		this.sendDate = sendDate;
+		this.senderPhone = sender;
+		this.receiverId = receiver;
+	}
+	
+	public MessageEntity(String content, Date sendDate, String sender, int receiver) {
+		this.content = content;
+		this.sendDate = sendDate;
+		this.senderPhone = sender;
+		this.receiverId = receiver;
 	}
 }
